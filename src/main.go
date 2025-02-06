fa21b3fbf51eda0747c734d7375bb0ec55badb6f
Tim Kempkens <tim.kempkens@googlemail.com>
2025-02-06 06:00:58 +0100

    added congestion control

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 )
 
+var detectedCongestions [][4]byte
+
 func main() {
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
 	if err != nil {
@@ -48,8 +50,37 @@ func process(buffer []byte, addr syscall.Sockaddr) {
 	ipparser.Print(ipPaket)
 
 	if ipPaket.Ecn == ipparser.CE {
-		// Setze den
+		if !ContainsFunc(detectedCongestions, ipPaket.SourceIP, compareIPs) {
+			detectedCongestions = append(detectedCongestions, ipPaket.SourceIP)
+		}
 	}
 	//Compute response:
 	//Solange mein Transport Layer Congestion Control nicht unterstützt, muss ich in der Response den Wert des ECN Flags auf 0 setzen.
+}
+
+func compareIPs(ip1 [4]byte, ip2 [4]byte) bool {
+	for i := 0; i < 4; i++ {
+		if ip1[i] != ip2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Contains[T comparable](slice []T, item T) bool {
+	for _, val := range slice {
+		if val == item {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsFunc[T any](slice []T, item T, compare func(T, T) bool) bool {
+	for _, val := range slice {
+		if compare(val, item) {
+			return true
+		}
+	}
+	return false
 }
