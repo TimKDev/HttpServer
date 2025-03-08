3ac6f9f2a186178b23536720a09f6efd3274c8a4
Tim Kempkens <tim.kempkens@googlemail.com>
2025-03-08 22:35:25 +0100

    added acks for tcp

--- src/tcp-receiver/tcp-receiver.go
+++ src/tcp-receiver/tcp-receiver.go
@@ -2,8 +2,8 @@ package tcpreceiver
 
 import (
 	"fmt"
-	tcpparser "http-server/tcp-parser"
-	tcpsender "http-server/tcp-sender"
+	"http-server/tcp-parser"
+	"http-server/tcp-sender"
 	"log"
 	"slices"
 )
@@ -48,12 +48,13 @@ func HandleTcpSegment(tcpPackage []byte, ipPseudoHeaderData *tcpparser.IPPseudoH
 		return nil
 	}
 
-	fmt.Println("Received TCP Package:")
-	tcpparser.PrintTcpSegment(tcpSegment)
-
 	//Syn => Erstellt eine neue TCP Session und schickt ein SYNACK
 	if tcpSegment.Flags == tcpparser.TCPFlagSYN {
-		handleSYN(*tcpSegment, ipPseudoHeaderData, config)
+		fmt.Println("Found Syn")
+		//handleSYN(*tcpSegment, ipPseudoHeaderData, config)
+	}
+	if tcpSegment.Flags == tcpparser.TCPFlagACK {
+		handleACK(*tcpSegment, ipPseudoHeaderData, config)
 	}
 
 	return nil
@@ -91,7 +92,7 @@ func handleSYN(tcpSegment tcpparser.TCPSegment, ipPseudoHeaderData *tcpparser.IP
 		Flags:           tcpparser.TCPFlagSYN | tcpparser.TCPFlagACK,
 		WindowSize:      serverWindowSize,
 		UrgentPtr:       0,
-		Options:         tcpSegment.Options, // Copy options from request
+		Options:         make([]byte, 0),
 		Payload:         make([]byte, 0),
 	}
 
@@ -99,7 +100,64 @@ func handleSYN(tcpSegment tcpparser.TCPSegment, ipPseudoHeaderData *tcpparser.IP
 }
 
 func handleACK(tcpSegment tcpparser.TCPSegment, ipPseudoHeaderData *tcpparser.IPPseudoHeaderData, config TcpHandlerConfig) {
+	fmt.Println("Handle Ack")
+	session := FindSession(ipPseudoHeaderData.SourceIP, tcpSegment.SourcePort)
+	if session == nil {
+		return
+	}
 
+	fmt.Println("Found session")
+	session.State = ConnectionEstablished
+
+	if len(tcpSegment.Payload) != 0 {
+		session.ReceivedSegments = append(session.ReceivedSegments, TcpDataSegments{
+			SequenceNumber: tcpSegment.SequenceNumber,
+			Payload:        tcpSegment.Payload,
+		})
+
+		fmt.Println("Send response")
+
+		currentAck := getCurrentAckNum(session)
+
+		ackRes := tcpparser.TCPSegment{
+
+			SourcePort:      uint16(config.Port),
+			DestinationPort: tcpSegment.SourcePort,
+			SequenceNumber:  session.LastSendSequenceNumber,
+			AckNumber:       currentAck,
+			Flags:           tcpparser.TCPFlagACK,
+			WindowSize:      session.ServerWindowSize,
+			UrgentPtr:       0,
+			Options:         make([]byte, 0),
+			Payload:         make([]byte, 0),
+		}
+
+		tcpsender.SendTCPSegment(ipPseudoHeaderData.DestinationIP, ipPseudoHeaderData.SourceIP, &ackRes)
+	}
+}
+
+func getCurrentAckNum(session *TcpSession) uint32 {
+	ackNumRes := session.LastSendAck
+	for _, data := range session.ReceivedSegments {
+		dataAckNum := data.SequenceNumber + uint32(len(data.Payload))
+		if dataAckNum < ackNumRes {
+			continue
+		}
+		ackNumRes = dataAckNum
+	}
+	return ackNumRes
+}
+
+func FindSession(destinationIP [4]byte, destinationPort uint16) *TcpSession {
+	idx := slices.IndexFunc(sessions, func(s *TcpSession) bool {
+		return s.DestinationPort == destinationPort &&
+			slices.Equal(s.DestinationIP[:], destinationIP[:])
+	})
+
+	if idx == -1 {
+		return nil
+	}
+	return sessions[idx]
 }
 
 func AddSession(session *TcpSession) error {
